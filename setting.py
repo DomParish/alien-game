acddd6aecc84324f5a617ce623361a28cfc07089
Dom <dom.l.parish@icloud.com>
2023-08-13 22:41:26 -0500

    screen size changes

--- setting.py
+++
@@ -1,8 +0,0 @@
-"""In game settings class to handle game settings"""
-class Settings:
-    def __init__(self):
-        """Initialize the settings"""
-        # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (255, 230, 230)
--- settings.py
+++ settings.py
@@ -3,6 +3,6 @@ class Settings:
     def __init__(self):
         """Initialize the settings"""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1600
+        self.screen_height = 1200
         self.bg_color = (255, 230, 230)
